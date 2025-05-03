e7686fa203f11af0608b4b6e1fc49e1af1877a61
Flávio Farias <flavio.farias@ccc.ufcg.edu.br>
2025-05-03 02:31:58 -0300

    Fix: home layout is not a module error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Footer from "@/app/pages/shared/components/Footer";
 export default function Index() {
   return (
     <div className="flex flex-col min-h-screen p-[500-20-0] gap-16 sm:p-[50_20_0] font-[family-name:var(--font-geist-sans)]">
-      {/* <Header /> */}
       <HomePage />
       <Footer />
     </div>
